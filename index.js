03c31aa38908424d3c87cd748ee202362993277a
Priyanka Soni <Priyankasoni2016@gmail.com>
2023-09-25 15:34:26 +0530

    models/Url.js further written & index.js and routes/url.js modified

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ connectDB();
 
 app.use(express.json({ extended: false}));
 
+// Define Routes
+
+app.use('/', require('./routes/index'));
+
+app.use('/api/url', require('./routes/url'));
+
 const PORT = 3000;
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));

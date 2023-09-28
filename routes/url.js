c59743a18d4f1e248b34ad97e1c41ba7e4d32aa0
Priyanka Soni <Priyankasoni2016@gmail.com>
2023-09-28 16:34:41 +0530

    routes/url.js modified

--- routes/url.js
+++ routes/url.js
@@ -6,4 +6,11 @@ const config = require('config');
 
 const Url = require('../models/Url');
 
+//@route      POST /api/url/shorten
+// @desc       Create short URL
+router.post('/shorten', (req, res) => {
+    const {longUrl } = req.body;
+    const baseUrl =    config.get('baseUrl');
+
+});
 module.exports = router;

44a2936772081975371cdfc269192779d03efed4
Priyanka Soni <Priyankasoni2016@gmail.com>
2024-02-07 22:46:24 +0530

    routes/index.js modified

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,19 @@ route.get('/:code', async (req, res) => {
     try{
         const url = await Url.findOne({ urlCode: req.params.code });
 
-    }
-     
+        if(url)
+          {
+            return res.redirect(url.longurl);
+          } else
+          {
+            return res.status(404).json('No url found');
+          }
+        }
+         catch (err)
+            {
+        console.error(err);
+        res.status(500).json('Server error');
+            }  
 });
 
 module.exports = router;
